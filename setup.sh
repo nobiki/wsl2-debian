5d41c28abd47df6c01ff7df5e6a3f10459ab9d35
Naoaki Obiki <obiki.wk@gmail.com>
2020-05-31 03:25:07 +0900

    apt

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eu
+cd $(dirname `realpath $0`)
+
 export DEBIAN_FRONTEND=noninteractive
 DOTFILES_REPOS=git@github.com:nobiki/dotfiles
 
@@ -28,7 +30,54 @@ function run() {
     kill -9 $SPIN_PID
 }
 
+sudo apt-get update
+
 run shell/apt/core
 run shell/dotfiles
 run shell/locale-ja
 
+run shell/apt/common
+run shell/apt/development
+
+# shell/anyenv
+# shell/awscli
+# shell/bat
+# shell/certbot
+# shell/chrome
+# shell/ctags
+# shell/direnv
+# shell/docker-ce
+# shell/dotfiles
+# shell/eslint
+# shell/exa
+# shell/font
+# shell/fzf
+# shell/gcloud
+# shell/git_find_big
+# shell/goenv
+# shell/hadolint
+# shell/hcat
+# shell/helm
+# shell/ibus-mozc
+# shell/jmeter
+# shell/jq
+# shell/js-beautify
+# shell/locale-ja
+# shell/nginx
+# shell/ngrok
+# shell/nodenv
+# shell/phpcs
+# shell/phpenv
+# shell/postman
+# shell/pyenv
+# shell/rbenv
+# shell/ripgrep
+# shell/rsense
+# shell/sql-formatter
+# shell/stacer
+# shell/trans
+# shell/vim
+# shell/vlc
+# shell/wine
+# shell/xpanes
+# shell/yarn
--- shell/dotfiles.sh
+++ shell/dotfiles.sh
@@ -1,2 +1,3 @@
+sudo rm -rf /tmp/dotfiles
 git clone --quiet $DOTFILES_REPOS /tmp/dotfiles
 /tmp/dotfiles/copy.sh
